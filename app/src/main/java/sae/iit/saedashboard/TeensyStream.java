784e81a4a4f6f3e020627116e7e8431b4d3c8d0e
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-05 12:28:49 -0600

    Better syntax, better documentation, papa johns

--- app/src/main/java/sae/iit/saedashboard/TeensyStream.java
+++ app/src/main/java/sae/iit/saedashboard/TeensyStream.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class TeensyStream {
-    private final HashMap<Long, Long> Teensy_Data = new HashMap<>();
+    private final HashMap<Long, msgBlock> Teensy_Data = new HashMap<>();
     private final String FILENAME_SAVE = "TEENSY_JSON_MAP.json";
     private final TeensyLogBooleanCallback callOnLoad;
     private final String LOG_TAG = "Teensy Stream";
@@ -39,23 +39,89 @@ public class TeensyStream {
     private HashMap<Integer, String> Teensy_LookUp_Str = new HashMap<>();
     private FileOutputStream logFile;
 
+    private boolean enableLogCallback = true;
+    private boolean enableLogFile;
     private boolean JSONLoaded = false;
-    private boolean hexLog = false;
+    private boolean hexMode = false;
 
     // region Interfaces
 
+    /**
+     * Simple callback class, used in multiple functions
+     */
     public interface TeensyCallback extends USBSerial.DeviceActionCallback {
     }
 
+    /**
+     * Callback class which accepts a long, typically the value of a message
+     */
+    public interface TeensyLongCallback {
+        void callback(long num);
+    }
+
+    /**
+     * Callback class which accepts a string, typically the string interpretation of a value
+     */
     public interface TeensyLogCallback {
         void callback(String msg);
     }
 
+    /**
+     * Callback class which accepts a boolean, typically whether a JSON map was successfully loaded
+     */
     public interface TeensyLogBooleanCallback {
         void callback(boolean jsonLoaded);
     }
 
     // endregion
+
+    // region Getter/Setters
+
+    /**
+     * @return Whether the logging callback is being called at all
+     */
+    public boolean isEnableLogCallback() {
+        return enableLogCallback;
+    }
+
+    /**
+     * @param enableLogCallback Set whether the logging callback should be called at all
+     */
+    public void setEnableLogCallback(boolean enableLogCallback) {
+        this.enableLogCallback = enableLogCallback;
+    }
+
+    /**
+     * @return Whether received data is being logged to a file
+     */
+    public boolean isEnableLogFile() {
+        return enableLogFile;
+    }
+
+    /**
+     * @param enableLogFile Set whether received data should be logged to a file
+     */
+    public void setEnableLogFile(boolean enableLogFile) {
+        this.enableLogFile = enableLogFile;
+    }
+
+    /**
+     * @return Whether the logging callback should be given a hex representation of data instead
+     */
+    public boolean isHexMode() {
+        return hexMode;
+    }
+
+    /**
+     * @param hexMode Set whether the logging callback should be given a hex representation of data instead
+     */
+    public void setHexMode(boolean hexMode) {
+        this.hexMode = hexMode;
+    }
+
+    // endregion
+
+    // region Enums
 
     /**
      * Commands that can be sent through the `write` function
@@ -65,6 +131,31 @@ public class TeensyStream {
         public static final byte[] CLEAR_FAULT = {45};
     }
 
+    /**
+     * When to update enums for message callbacks
+     */
+    public enum UPDATE {
+        ON_RECEIVE,
+        ON_VALUE_CHANGE,
+        ON_VALUE_DECREASE,
+        ON_VALUE_INCREASE,
+    }
+
+    // endregion
+
+    /**
+     * TeensyStream gives the ability to communicate with a Teensy 3.6 over USB serial
+     * <p>
+     * Given a properly formatted JSON, TeensyStream can also interpret data in 8 byte chunks as string messages
+     * <p>
+     * TeensyStream mainly uses callbacks as a way of communication
+     *
+     * @param activity     The main activity
+     * @param logMessage   The callback to be used for logging a value's string interpretation to UI
+     * @param deviceAttach The callback to be called when a device is attached
+     * @param deviceDetach The callback to be called when a device is detached
+     * @param callOnLoad   The callback to be called when a new JSON map has been updated
+     */
     public TeensyStream(Activity activity, TeensyLogCallback logMessage, TeensyCallback deviceAttach, TeensyCallback deviceDetach, TeensyLogBooleanCallback callOnLoad) {
         Log.i(LOG_TAG, "Making teensy stream");
         this.callOnLoad = callOnLoad;
@@ -83,46 +174,84 @@ public class TeensyStream {
             Toaster.showToast("Failed to open new file for teensy logging", true);
         }
 
-        UsbSerialInterface.UsbReadCallback streamCallback;
+        enableLogFile = logFile != null;
 
-        if (logFile != null) {
-            streamCallback = arg0 -> {
+        UsbSerialInterface.UsbReadCallback streamCallback = arg0 -> {
+            if (enableLogFile)
                 try {
                     logFile.write(arg0);
                 } catch (Exception ignored) {
                 }
-                String msg = setData(arg0);
+            if (enableLogCallback) {
+                String msg = processData(arg0);
                 if (msg.length() > 0) {
                     logMessage.callback(msg);
                 }
-            };
-        } else {
-            streamCallback = arg0 -> {
-                String msg = setData(arg0);
-                if (msg.length() > 0) {
-                    logMessage.callback(msg);
-                }
-            };
-        }
+            } else {
+                consumeData(arg0);
+            }
+        };
 
         serialConnection = new USBSerial(activity, streamCallback, deviceAttach, deviceDetach);
 
         loadLookupTable(activity);
     }
 
-    public void setHexMode(boolean enable) {
-        hexLog = enable;
+    // region Messaging
+
+    /**
+     * Class which deals with updating message values and callbacks
+     */
+    private static class msgBlock {
+        private TeensyLongCallback callbackFunc; // Callback functions
+        private long value = 0; // Store data
+        private UPDATE updateWhen = UPDATE.ON_RECEIVE;
+
+        void setCallback(TeensyLongCallback callback) {
+            this.callbackFunc = callback;
+        }
+
+        void setUpdate(UPDATE when) {
+            this.updateWhen = when;
+        }
+
+        void update(long val) {
+            long prevValue = this.value;
+            this.value = val;
+
+            if (callbackFunc != null)
+                switch (updateWhen) {
+                    case ON_RECEIVE:
+                        callbackFunc.callback(val);
+                        break;
+                    case ON_VALUE_CHANGE:
+                        if (prevValue != value)
+                            callbackFunc.callback(val);
+                        break;
+                    case ON_VALUE_DECREASE:
+                        if (prevValue > value)
+                            callbackFunc.callback(val);
+                        break;
+                    case ON_VALUE_INCREASE:
+                        if (prevValue < value)
+                            callbackFunc.callback(val);
+                }
+        }
+
     }
 
-    public boolean getHexMode() {
-        return hexLog;
-    }
-
-    public long requestData(long msgID) {
-        return Teensy_Data.containsKey(msgID) ? Teensy_Data.get(msgID) : 0;
-    }
-
-    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+    /**
+     * Helper function to get the key from a map using a value
+     * <p>
+     * Must be a 1:1 map
+     *
+     * @param map   The 1:1 map to look in
+     * @param value Value to find
+     * @param <T>   Map key type
+     * @param <E>   Map value type
+     * @return type T value
+     */
+    private static <T, E> T getKeyByValue(Map<T, E> map, E value) {
         for (Map.Entry<T, E> entry : map.entrySet()) {
             if (Objects.equals(value, entry.getValue())) {
                 return entry.getKey();
@@ -131,7 +260,44 @@ public class TeensyStream {
         return null;
     }
 
-    public long requestDataMappingID(String stringTag, String stringMsg) {
+    /**
+     * Return the value of a message
+     *
+     * @param msgID Runtime specific ID of a message
+     * @return The last value the message received, default 0, -1 if not found
+     */
+    public long requestData(long msgID) {
+        msgBlock msg = Teensy_Data.get(msgID);
+        return msg == null ? -1 : msg.value;
+    }
+
+    /**
+     * Set the callback for a specific message
+     * <p>
+     * Only one callback per message
+     *
+     * @param msgID    Runtime specific ID of a message
+     * @param callback Callback class
+     * @param when     When the callback should be called
+     */
+    public void setCallback(long msgID, TeensyLongCallback callback, UPDATE when) {
+        if (Teensy_Data.containsKey(msgID)) { // Only store value if it is needed
+            msgBlock msg = Teensy_Data.get(msgID);
+            msg.setCallback(callback);
+            msg.setUpdate(when);
+        } else {
+            Log.w(LOG_TAG, "Msg ID: " + msgID + " does not exist yet, no callback set");
+        }
+    }
+
+    /**
+     * Get the runtime specific ID of a message that will be received
+     *
+     * @param stringTag The exact tag that the message has
+     * @param stringMsg The exact string that the message has
+     * @return ID of the given message, -1 if not found
+     */
+    public long requestMsgID(String stringTag, String stringMsg) {
         if (!JSONLoaded) {
             Log.w(LOG_TAG, "JSON has not been loaded, unable to process request");
             return -1;
@@ -145,31 +311,63 @@ public class TeensyStream {
             mapping.putShort(tagID.shortValue());
             mapping.putShort(strID.shortValue());
             long msgID = mapping.getInt(0);
-            Teensy_Data.put(msgID, 0L);
+            Teensy_Data.put(msgID, new msgBlock());
             return msgID;
         }
 
         return -1;
     }
 
+    // endregion
+
+    // region Data Processing
+
+    /**
+     * Update requested values and run callbacks
+     *
+     * @param data_block 8 byte data block
+     * @return the separate IDs and values of a message
+     */
     private long[] updateData(byte[] data_block) {
         long[] IDs = ByteSplit.getTeensyMsg(data_block);
         long msgID = IDs[3];
         if (Teensy_Data.containsKey(msgID)) { // Only store value if it is needed
-            Teensy_Data.put(msgID, IDs[2]);
+            msgBlock msg = Teensy_Data.get(msgID);
+            msg.update(IDs[2]);
         }
         return IDs;
     }
 
     /**
-     * Set teensy data in a HashMap given a raw byte array
+     * Consume data, does not interpret anything about it
+     * <p>
+     * Should be faster than processData, but does not output anything
      *
-     * @param raw_data byte array
+     * @param raw_data received byte array
      */
-    private String setData(byte[] raw_data) { // Improve: run this on separate thread
+    private void consumeData(byte[] raw_data) {
+        for (int i = 0; i < raw_data.length; i += 8) {
+            byte[] data_block = new byte[8];
+            try {
+                System.arraycopy(raw_data, i, data_block, 0, 8);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                Log.w(LOG_TAG, "Received cutoff array");
+                continue;
+            }
+            updateData(data_block);
+        }
+    }
+
+    /**
+     * Both Consume and interpret raw data that has been received
+     *
+     * @param raw_data received byte array
+     * @return The interpreted string of the data
+     */
+    private String processData(byte[] raw_data) { // Improve: run this on separate thread
         StringBuilder output = new StringBuilder(32);
 
-        if (hexLog) {
+        if (hexMode) {
             for (int i = 0; i < raw_data.length; i += 8) {
                 byte[] data_block = new byte[8];
                 try {
@@ -207,19 +405,7 @@ public class TeensyStream {
         return output.substring(0, output.length() - 1);
     }
 
-    /**
-     * @return The string representation of the stored teensy messages
-     */
-    public String dataString() {
-//        StringBuilder str = new StringBuilder();
-//        for (Map.Entry<Long, Long> e : Teensy_Data.entrySet()) {
-//            str.append(e.getKey());
-//            str.append(" : ");
-//            str.append(ByteSplit.hexStr(e.getValue()).replaceAll("..(?!$)", "$0|"));
-//        }
-//        return str.toString();
-        return "";
-    }
+    // endregion
 
     // region Serial IO
 
@@ -361,5 +547,20 @@ public class TeensyStream {
         return true;
     }
     // endregion
+
+    /**
+     * @return The string representation of the stored teensy messages
+     */
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (Map.Entry<Long, msgBlock> e : Teensy_Data.entrySet()) {
+            str.append(e.getKey());
+            str.append(" : ");
+            str.append(e.getValue().value);
+            str.append('\n');
+        }
+        return str.toString();
+    }
 
 }
