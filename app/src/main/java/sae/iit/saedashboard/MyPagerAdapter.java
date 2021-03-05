0c146f6bd328b4342caa40b86e8dc0740a54aed8
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-05 02:00:16 -0600

    return same instance of fragments

--- app/src/main/java/sae/iit/saedashboard/MyPagerAdapter.java
+++ app/src/main/java/sae/iit/saedashboard/MyPagerAdapter.java
@@ -4,45 +4,54 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MyPagerAdapter extends FragmentStatePagerAdapter {
 
-	public MyPagerAdapter(FragmentManager fm) {
-		//Creates Fragment Adapter
-		super(fm);
-	}
+    MainTab p0;
+    SecondaryTab p1;
+    TroubleshootTab p2;
 
-	@Override
-	public Fragment getItem(int position) {
-		//Generates the new tab
-		switch (position) {
-			case 0:
-				return new MainTab();
-			case 1:
-				return new SecondaryTab();
-			case 2:
-				return new TroubleshootTab();
+    public MyPagerAdapter(FragmentManager fm) {
+        //Creates Fragment Adapter
+        super(fm, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        p0 = new MainTab();
+        p1 = new SecondaryTab();
+        p2 = new TroubleshootTab();
+    }
 
-		}
-		return null;
-	}
+    @Override
+    public @NotNull Fragment getItem(int position) {
+        //Generates the new tab
+        switch (position) {
+            case 0:
+                return p0;
+            case 1:
+                return p1;
+            case 2:
+                return p2;
 
-	@Override
-	public int getCount() {
-		return 3;
-	}
+        }
+        return p0;
+    }
 
-	@Override
-	public CharSequence getPageTitle(int position) {
-		switch (position) {
-			case 0:
-				return "Main";
-			case 1:
-				return "DataLogging";
-			case 2:
-				return "Troubleshooting";
-			default:
-				return null;
-		}
-	}
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Main";
+            case 1:
+                return "DataLogging";
+            case 2:
+                return "Troubleshooting";
+            default:
+                return null;
+        }
+    }
 
 }
